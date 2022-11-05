eb3f53c4544245d12a122662ce20cd0acf22a2de
gry10 <gry10@d3-hpc-sjtu-test-002.cm.cluster>
2022-11-05 09:17:13 +0800

    what

---
+++ hubert_finetune_small.sh
@@ -0,0 +1,20 @@
+export CUDA_VISIBLE_DEVICES=1
+
+
+/mnt/lustre/sjtu/home/gry10/anaconda3/envs/fairseq/bin/python fairseq_cli/hydra_train.py \
+--config-dir examples/hubert/config/finetune \
+--config-name base_10h \
+task.data=/data/ygr/small20 \
+task.label_dir=/data/ygr/small20 \
+task.labels=["ltr"] \
+model.w2v_path=/mnt/lustre/sjtu/home/xc915/data/fairseq/None/checkpoints/checkpoint20.pt \
+checkpoint.save_dir=/data/ygr/small20/f \
+distributed_training.distributed_world_size=1 \
+optimization.update_freq=[8] \
+dataset.validate_interval=1 \
+optimization.max_update=20 
+
+
+
+#checkpoint.restore_file=/mnt/lustre/sjtu/home/xc915/data/fairseq/None/checkpoints/checkpoint_best.pt
+# 20 audios for test
